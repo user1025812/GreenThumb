42a4c2df04061bde5df47bebd8822a1d7604836d
JerileeMonicRosales <jerileemonic05@gmail.com>
2026-05-17 17:53:33 +0800

    added home and made chnages to the router

---
+++ greenthumb/src/pages/Home.jsx
@@ -0,0 +1,49 @@
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
+import Hero from '../components/Hero';
+import Purpose from '../components/Purpose'
+import ThreeStepSection from '../components/ThreeStepSection';
+import HomeStats from '../components/HomeStats';
+import BiodiversitySection from '../components/BiodiversitySection';
+import { Outlet } from "react-router-dom";
+import InfoGrid, { InfoBlock } from "../components/InfoGrid";
+import iconCommunity from '../assets/Group2799.png';
+import iconNative from '../assets/Group2798.png';
+import iconSDG from '../assets/Group2800.png';
+
+import '../App.css'
+
+function Home() {
+  return (
+    <div className="min-h-screen bg-white font-sans text-gray-900">
+      <Navbar />
+      <Outlet />
+      <Hero />
+      <Purpose />
+      {/* <Features /> */}
+            <InfoGrid>
+        <InfoBlock 
+          title= "Community First"
+          description= "Partnering with local farmers for sustainable growth."
+          imgSrc= {iconCommunity}
+        />
+        <InfoBlock 
+          title= "Native Species Only" 
+          description= "We prioritize Narra, Molave, and Anitolo." 
+          imgSrc= {iconNative}
+        />
+        <InfoBlock 
+          title= "SDG Aligned"
+          description= "Contributing to 6 Global Sustainable Development Goals." 
+          imgSrc= {iconSDG}
+        />
+      </InfoGrid>
+      <ThreeStepSection />
+      <HomeStats />    
+      <BiodiversitySection />
+      <Footer />
+    </div>
+  );
+}
+
+export default Home;
--- greenthumb/src/router.jsx
+++ greenthumb/src/router.jsx
@@ -1,12 +1,13 @@
 import { createBrowserRouter } from "react-router-dom";
-
+//used ai to navigate automatically to home rather than the app.jsx
+import Home from "./pages/Home";
 import App from "./App";
 import Join from "./pages/Join";
 import PlantATree from "./pages/PlantATree";
 import Progress from "./pages/Progress";  
 import Track from "./pages/Track";
-// import AboutUs from "./pages/AboutUs";
-// import Faqs from "./pages/Faqs";
+import AboutUs from "./pages/AboutUs";
+import Faqs from "./pages/Faqs";
 
 export const router = createBrowserRouter([
   {
@@ -28,14 +29,22 @@ export const router = createBrowserRouter([
     {
       path: "/track",
       element: <Track />,
+    }, 
+    {
+      path: "/aboutus",
+      element: <AboutUs />,
     },
-  
-  // {
-  //   path: "/aboutus",
-  //   element: <AboutUs />,
-  // },
-  // {
-  //   path: "/faqs",
-  //   element: <Faqs />,
-  // },
+    {
+      path: "/faqs",
+      element: <Faqs />,
+    },
+    {
+      path: "/",
+      element: <Navigate to="/home" replace /> 
+    },
+    {
+      path: "/home",
+      element: <Home />,
+    }
+
 ]);
